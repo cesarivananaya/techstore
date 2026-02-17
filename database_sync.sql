8537d42a001b8cf10e694209ebaaff8c530c769b
cesarivananaya <cesar.anaya@plai.mx>
2026-02-17 17:28:49 -0600

    Automatización de sincronización de DB habilitada

---
+++ database_sync.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict pf2l8h9qOl7g6SVAUa77x5xTqOMiug8rVRNP7kOx1j8f70708EqxUdL6RAoyT3o
+
+-- Dumped from database version 15.16 (Debian 15.16-1.pgdg13+1)
+-- Dumped by pg_dump version 15.16 (Debian 15.16-1.pgdg13+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict pf2l8h9qOl7g6SVAUa77x5xTqOMiug8rVRNP7kOx1j8f70708EqxUdL6RAoyT3o
+
